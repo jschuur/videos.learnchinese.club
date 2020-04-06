31780333b587d13f0a483f4b04fd4dde38c6a61d
Joost Schuur <jschuur@jschuur.com>
2020-04-06 14:26:27 +0100

    Switch to serverless-webpack

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const slsw = require('serverless-webpack');
+var ContextReplacementPlugin = require("webpack/lib/ContextReplacementPlugin");
+
+module.exports = {
+  target: 'node',
+  entry: slsw.lib.entries,
+  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  node: false,
+  optimization: {
+    minimize: slsw.lib.webpack.isLocal ? false : true,
+  },
+  devtool: 'inline-cheap-module-source-map',
+  stats: {
+    warningsFilter: [
+      'mongodb-client-encryption',
+      'saslprep'
+    ],
+    modules: false,
+    entrypoints: false
+  },
+  plugins: [
+    new ContextReplacementPlugin(/.*/)
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  { targets: { node: '12' }, useBuiltIns: 'usage', corejs: 3 }
+                ]
+              ]
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
