688ca1e02904421e49ce32b4aa380de6bafcdc13
Joost Schuur <jschuur@jschuur.com>
2020-04-06 09:15:58 +0100

    Layout component typo fix

--- site/src/templates/video-archive-template.js
+++ site/src/templates/video-archive-template.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
-import Layout from '../components/layout'
+import Layout from '../components/Layout'
 import VideoList from '../components/VideoList';
 import Pagination from '../components/Pagination';
 
