e3c698452f65a56c62f78f9dcef7c6106832f96f
Joost Schuur <jschuur@jschuur.com>
2020-04-13 12:36:51 +0100

    Basic 404 page

---
+++ site/src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+import Layout from '../components/Layout';
+import SEO from '../components/SEO';
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      <SEO title="404 Page not found" />
+
+      <p>Oops, page not found!</p>
+      <p><Link to='/'>Go Back</Link></p>
+    </Layout>
+  );
+};
