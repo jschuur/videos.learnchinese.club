f7becfc8621fbb2b0935eda0ee6983616c1e51e1
Joost Schuur <jschuur@jschuur.com>
2020-04-13 10:43:35 +0100

    Trigger GitHub Actions rebuild via repository dispatch webhook

---
+++ utils/rebuild.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+const { resolve } = require('path');
+const axios = require('axios');
+
+dotenv.config({ path: resolve(__dirname, '../.env.development') });
+
+const DISPATCH_URL = 'https://api.github.com/repos/jschuur/videos.learnchinese.club/dispatches'
+
+console.log(`Triggering GitHub Action rebuild...`);
+
+axios.post(DISPATCH_URL, { event_type: "rebuild" }, {
+  headers: {
+    'Accept': 'application/vnd.github.everest-preview+json',
+    'Authorization': `token ${process.env.GITHUB_PERSONAL_TOKEN}`
+  }
+})
+.then(function (response) {
+  console.log(`Done (${response.status}: ${response.statusText})\nCheck latest workflow: https://github.com/jschuur/videos.learnchinese.club/actions?query=workflow%3A%22Redeploy+Gatsby+site%22`);
+})
+.catch(function (err) {
+  console.error(`Problem triggering webhook: ${err.message}`);
+});
